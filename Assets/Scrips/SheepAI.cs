decd8df1249e2baf594552a714083743c0383c13
André Baptista <aca.baptista@gmail.com>
2019-04-25 04:09:48 +0100

    WIP: sheep AI movement

---
+++ Assets/Scrips/SheepAI.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class SheepAI : MonoBehaviour
+{ 
+    private List<GameObject> sheeps = new List<GameObject>();
+    private float timer = TIME_BETWEEN_MOVEMENT;
+
+    enum State { Ready, Busy };
+    private const float TIME_BETWEEN_MOVEMENT = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            updateSheeps();
+            moveSheeps();
+            resetTimer();
+        }
+    }   
+
+    void updateSheeps()
+    {
+        sheeps.Clear();
+        foreach (Transform child in transform)
+        {
+            sheeps.Add(child.gameObject);
+        }
+    }
+
+    void moveSheeps()
+    {
+
+        int numSheepsToMove = getNumSheepsToMove(sheeps.Count);  //get the number of sheeps to move based on the total ammount of sheeps in play
+        List<int> randomSheepIndexes = calculateRandomIndexes(numSheepsToMove);
+
+        GameObject[] sheepsToMove = new GameObject[numSheepsToMove];
+        for(int i = 0; i < numSheepsToMove; i++)
+            sheepsToMove[i] = sheeps[randomSheepIndexes[i]];
+
+        foreach (GameObject sheep in sheepsToMove)
+        {
+            var sheep_script = sheep.GetComponent<SheepMovement>();
+
+            //TO DO: definir 3 variavieis direcao, distancia e tempo parado
+            //passar ao move as 3 variaveis
+
+            sheep_script.move();
+        }
+    }
+
+    void resetTimer()
+    {
+        timer = TIME_BETWEEN_MOVEMENT;
+    }
+
+    int getNumSheepsToMove(int totalSheeps)
+    {
+        return 2; //TO DO (% of total Sheeps)
+    }
+
+    List<int> calculateRandomIndexes(int numSheepsToMove)
+    {
+        List<int> randomIndexes = new List<int>();
+        System.Random rnd = new System.Random();
+
+        for(int i = 0; i < numSheepsToMove; i++)
+        {
+            int number;
+            do
+            {
+                number = rnd.Next(0, sheeps.Count);
+            }
+            while (randomIndexes.Contains(number));
+
+            randomIndexes.Add(number);
+        }
+
+        return randomIndexes;
+    }
+
+
+
+    /*bool hasValue(int[] array, int value)
+    {
+        if (array.Length == 0)
+            return false;
+
+        for(int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == value)
+                return true;
+        }
+        return false;
+    }*/
+}
--- Assets/Scrips/SheepMovement.cs
+++ Assets/Scrips/SheepMovement.cs
@@ -5,25 +5,42 @@ using UnityEngine;
 public class SheepMovement : MonoBehaviour
 {
     public Transform sheep_transform;
-    private const int refreshRate = 10;
-    private const float stepSize = 0.25f;
-    private int timeUntilUpdate = refreshRate;
+    public Rigidbody rigidbody;
+    private int state = (int) State.Ready;
 
+    enum State { Ready, Busy };
+
+    private const float stepSize = 0.25f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        timeUntilUpdate -= 1;
+
+
+        //rigidbody.AddForce(new Vector3(50, 0, 0), ForceMode.Acceleration);
+
+        /*timeUntilUpdate -= 1;
 
         if(timeUntilUpdate <= 0){
             sheep_transform.position = sheep_transform.position + new Vector3(stepSize,0f,0f);
             timeUntilUpdate = refreshRate;
-        }
+        }*/
     }
+
+    public void move()
+    {
+        Debug.Log("called move");
+    }
+
+    public int getState()
+    {
+        return state;
+    }
+
 }
