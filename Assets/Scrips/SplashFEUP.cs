c8809a5a3713e66074435ef804679dd8f30013af
ruimoliveira <ruimiguelalmeidaoliveira@gmail.com>
2019-05-20 16:19:05 +0100

    Created scene to show FEUP logo after Unity logo

---
+++ Assets/Scrips/SplashFEUP.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashFEUP : MonoBehaviour
+{
+    public Image feupLogo;
+
+    IEnumerator Start()
+    {
+        feupLogo.canvasRenderer.SetAlpha(0.0f);
+        FadeIn();
+        yield return new WaitForSeconds(2.5f);
+        FadeOut();
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(1);
+    }
+
+    void FadeIn()
+    {
+        feupLogo.CrossFadeAlpha(1.0f, 0.5f, false);
+    }
+
+    void FadeOut()
+    {
+        feupLogo.CrossFadeAlpha(0.0f, 0.5f, false);
+
+    }
+}
